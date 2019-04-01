1c0a3ed2f3f73729c041b2dd0abe81c7cfa45f7a
Austin <berenyi.austin@gmail.com>
2019-04-01 15:01:51 -0600

    Initial Commit

---
+++ BoxCutter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BoxCutter
+//
+//  Created by Austin Berenyi on 4/1/19.
+//  Copyright © 2019 Austin Berenyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
