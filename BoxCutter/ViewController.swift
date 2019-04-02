92e00a2509ac466c3d0142598aee6f7db67038e6
Austin <berenyi.austin@gmail.com>
2019-04-02 11:36:29 -0600

    commit

--- BoxCutter/ViewController.swift
+++ BoxCutter/ViewController.swift
@@ -9,43 +9,57 @@
 import UIKit
 import PDFKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIScrollViewDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     @IBOutlet var lengthTextField: UITextField!
     @IBOutlet var widthTextField: UITextField!
     @IBOutlet var heightTextField: UITextField!
     
     @IBOutlet var faceA: FaceView!
     @IBOutlet var faceB: FaceView!
+    @IBOutlet weak var faceA1: FaceView!
+    @IBOutlet weak var faceB1: FaceView!
+    
+    @IBOutlet weak var faceAHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var faceAWidthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var faceBWidthConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var faceA1HeightConstraint: NSLayoutConstraint!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lengthTextField.delegate = self
+        widthTextField.delegate = self
+        heightTextField.delegate = self
+        scrollView.delegate = self
+        
+        faceA.translatesAutoresizingMaskIntoConstraints = false
+        faceB.translatesAutoresizingMaskIntoConstraints = false
+        
+        scrollView.autoresizingMask = [UIView.AutoresizingMask.flexibleWidth,UIView.AutoresizingMask.flexibleHeight]
+        scrollView.minimumZoomScale = 0.01
+        scrollView.minimumZoomScale = 50
+        scrollView.zoomScale = 1
+        
+        self.setupGestureRecognizer()
+        
+    }
+    
+    func setupGestureRecognizer() {
         
     }
     
     @IBAction func viewPDF(_ sender: Any) {
         
-        guard let length = NumberFormatter().number(from:
-            lengthTextField.text ?? "") else { return }
-        
-        guard let width = NumberFormatter().number(from:
-            widthTextField.text ?? "") else { return }
-        
-        guard let height = NumberFormatter().number(from:
-            heightTextField.text ?? "") else { return }
-        
-        faceA.widthAnchor.constraint(equalToConstant: CGFloat(truncating: length))
-        faceA.heightAnchor.constraint(equalToConstant: CGFloat(truncating: height))
-        faceB.widthAnchor.constraint(equalToConstant: CGFloat(truncating: width))
-        
-        view.layoutIfNeeded()
-        view.layoutSubviews()
-        
-        createPdfFromView(aView: self.view, saveToDocumentsWithFileName: "MC.pdf")
+        createPdfFromView(aView: self.scrollView.subviews[0], saveToDocumentsWithFileName: "MC.pdf")
         
         // Create and add a PDFView to the view hierarchy.
-        let pdfView = PDFView(frame: view.bounds)
+        let pdfView = PDFView(frame: self.scrollView.subviews[0].bounds)
         pdfView.autoScales = true
         view.addSubview(pdfView)
         
@@ -77,6 +91,28 @@ class ViewController: UIViewController, UITextFieldDelegate {
             let documentsFileName = documentDirectories + "/" + fileName
             debugPrint(documentsFileName)
             pdfData.write(toFile: documentsFileName, atomically: true)
+        }
+    }
+    
+    @IBAction func updateDims(_ sender: Any) {
+        
+        guard let length = NumberFormatter().number(from:
+            lengthTextField.text ?? "") else { return }
+        
+        guard let width = NumberFormatter().number(from:
+            widthTextField.text ?? "") else { return }
+        
+        guard let height = NumberFormatter().number(from:
+            heightTextField.text ?? "") else { return }
+        
+        let flapHeight = Int(truncating: width)/2
+        
+        UIView.animate(withDuration: 0.3) {
+            self.faceAWidthConstraint.constant = CGFloat(truncating: length)
+            self.faceAHeightConstraint.constant = CGFloat(truncating: height)
+            self.faceBWidthConstraint.constant = CGFloat(truncating: width)
+            self.faceA1HeightConstraint.constant = CGFloat(flapHeight)
+            self.view.layoutIfNeeded()
         }
     }
 }
